b900a64ad1534f68ef3b011a933dd807ed2f60f4
andreram <iramkairsingh@gmail.com>
2021-06-14 18:31:47 -0400

    Add source copy

--- src/react/Data.ts
+++ src/react/Data.ts
@@ -8,9 +8,11 @@ export interface Module {
     name: string;
     subject_name: Subject;
   }[];
+  group?: string; 
 }
 
 const commonGroupCopyStart = 'This section contains learning resources';
+const standardizedTestsCopy = 'This section contains practice US Standardized Tests and answer keys. These resources are intended for students in advanced years of secondary school, espeically those preparing for further education or university.';
 
 const commonSubjectCopyStart = (subject: string) =>
   `Welcome to ${subject}! Here, you will find a wide range of learning content about`;
@@ -26,13 +28,30 @@ export enum Subject {
   SocialSciences = 'Social Sciences',
 }
 
+export enum Source {
+  AfricanStorybooks = 'African Storybooks',
+  CK12 = 'CK-12 Textbooks',
+  GCF = 'GCF Learnfree',
+  GBOFW = 'Great Books of the World',
+  HesperianHealth = 'Hesperian Health Guides',
+  Infonet = 'Infonet - Biovision',
+  InteractiveWorldMap = 'Interactive World Map',
+  KALite = 'KA Lite Essentials',
+  MedlinePlus = 'Medline Plus',
+  MusicTheory = 'Music Theory',
+  PracticalAction = 'Practical Action',
+  Wikipedia = 'Wikipedia for Schools',
+  Wikivoyage = 'Wikivoyage',
+  Wiktionary = 'Wiktionary',
+}
+
 export const Math = {
   'Foundational Mathematics': `${commonGroupCopyStart} that help to build a strong understanding of basic Mathematics.`,
   'Algebra': `${commonGroupCopyStart} that help to understand equations and how to solve them.`,
   'Geometry': `${commonGroupCopyStart} about shapes, spaces and sizes.`,
   'Statistics & Probability': `${commonGroupCopyStart} that help to understand probability and data analysis.`,
   'Calculus':`${commonGroupCopyStart} that help to understand calculus at a basic level and at an advanced level.`,
-  'Tests & Textbooks': 'This section contains practice US Standardized Tests and answer keys. These resources are intended for students in advanced years of secondary school, especially those preparing for further education or university.',
+  'Tests & Textbooks': standardizedTestsCopy,
 }
 
 export const Science = {
@@ -56,7 +75,7 @@ export const Arts = {
 
 export const Literature = {
   'Fiction & Non-Fiction': 'This section contains non-fiction and fiction books in a variety of genres.',
-  'Tests & Textbooks': 'This section contains practice US Standardized Tests and answer keys. These resources are intended for students in advanced years of secondary school, especially those preparing for further education or university.',
+  'Tests & Textbooks': standardizedTestsCopy,
   'Reading & Spelling': `${commonGroupCopyStart} that help to improve reading and spelling skills.`,
 }
 
@@ -122,6 +141,45 @@ export const SubjectCopy = {
     copy: 'Welcome to Languages! Here, you will be able to practice and learn about languages from around the world.',
     tags: Languages,
   },
+};
+
+export const SourceCopy = {
+  [Source.KALite]: 'Khan Academy Lite Essentials includes thousands of video lessons and exercises on mathematics, science, and more. This incredible learning resource is brought to you by Khan Academy and The Foundation for Learning Equality. You can create an account to track your progress. When you return, you can login and view your progress.',
+  [Source.GBOFW]: 'A selection of over 400 classic, well-known books from around the world, provided by Project Gutenberg.',
+  [Source.AfricanStorybooks]: 'These stories are in 11 languages spoken in many countries in Southern and Eastern Africa. Reading these short, fun books are a perfect way to practice reading and language learning at the same time.',
+  [Source.CK12]: 'High-quality textbooks about Science, Technology, Engineering and Mathematics from ck12.org.',
+  [Source.GCF]: 'A collection of hundreds of illustrated articles and videos about technology, job training, reading, and math skills, produced by the Goodwill Community Foundation.',
+  [Source.PracticalAction]: 'Practical Action provides information on a wide range of agricultural, environmental, and health topics.',
+  [Source.Infonet]: 'Well-organized, illustrated information on a wide range of agricultural, environmental, and health topics.',
+  [Source.MedlinePlus]: 'A collection of more than 4,000 imedical articles from the U.S. National Library of Medicine and the National Institutes of Health. These materials do not provide medical advice and are for informational purposes only. This content is not intended to be a substitute for professional medical advice, diagnosis or treatment. Always seek the advice of a qualified health provider with any questions you may have regarding a medical condition. Never disregard professional medical advice or delay in seeking it because of something you have read here.',
+  [Source.MusicTheory]: 'Comprehensive music theory lessons and exercises to begin your journey into the realm of music from musictheory.net.',
+  [Source.HesperianHealth]: 'A collection of detailed, illustrated guides on healthcare in remote areas where access to doctors and facilities is limited. These materials do not provide medical advice and are for informational purposes only. This content is not intended to be a substitute for professional medical advice, diagnosis or treatment. Always seek the advice of a qualified health provider with any questions you may have regarding a medical condition. Never disregard professional medical advice or delay in seeking it because of something you have read here.',
+  [Source.Wikipedia]: 'A collection of 6,000 articles and 50,000 images on hundreds of topics and subjects from Wikipedia for Schools.',
+  [Source.Wikivoyage]: 'A worldwide travel guide created by Kiwix with information on destinations around the world. Make geography come alive!',
+  [Source.Wiktionary]: 'Wiktionary is a multilingual dictionary which aims to describe all words of all languages using definitions and descriptions in English.',
+  [Source.InteractiveWorldMap]: 'Explore Earth with this interactive and searchable world map from OpenStreetMaps and the XSCE project.',
+};
+
+export const getSourceGroupCopy = (group: string, source: string) => {
+  if (source === 'African Storybooks') {
+    return 'This section contains short books that help to improve a number of languages spoken across Southern and Eastern Africa.';
+  }
+
+  if (group === 'Standardized Tests') {
+    return standardizedTestsCopy;
+  }
+
+  let subject = group.replaceAll('&', 'and').toLowerCase();
+
+  if (subject.endsWith('age')) {
+    subject += ' groups';
+  } else if (subject.endsWith('subject')) {
+    subject += ' area'
+  } else if (['Humanities', 'Digital World', 'Environment'].includes(group)) {
+    subject = 'the ' + subject;
+  }
+
+  return `Learn more about ${subject} with this content from ${source}.`;
 }
 
 export enum Display {
@@ -150,4 +208,4 @@ export interface DataObject {
 }
 
 export const subjects = Object.values(Subject);
-export const sources = ['African Storybooks', 'CK-12 Textbooks', 'GCF Learnfree', 'Great Books of the World', 'Hesperian Health Guides', 'Infonet - Biovision', 'Interactive World Map', 'KA Lite Essentials', 'Medline Plus', 'Music Theory', 'Practical Action', 'Wikipedia for Schools', 'Wikivoyage', 'Wiktionary'];
+export const sources = Object.values(Source);
